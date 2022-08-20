16605b584cb829aae3be7af60be26dac054d0350
Hassan Abdellah <hassanabdellah25@gmail.com>
2022-08-20 02:03:15 +0200

    animating the linear gradient and add responsiveness to faqs section

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import './card.css';
-const Card = ({price,isAnual}) => {
+import React from "react";
+import "./card.css";
+const Card = ({ price, isAnual }) => {
   return (
-    <div className='card'>
-        <p>{price.type}</p>
-        <h1>$ {isAnual ? price.annually :price.monthly}</h1>
-        <ul>
-            {price.features.map((feature,index) => (
-                <li key={index} className={feature.included ? 'included' : ''}>{feature.name}</li>
-            ))}
-        </ul>
-        <button className='btn btn-block'>Learn more</button>
+    <div className="card">
+      <p>{price.type}</p>
+      <h1>$ {isAnual ? price.annually : price.monthly}</h1>
+      <ul>
+        {price.features.map((feature, index) => (
+          <li key={index} className={feature.included ? "included" : ""}>
+            {feature.name}
+          </li>
+        ))}
+      </ul>
+      <button className="btn btn-block">
+        <span>Learn more</span>
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Card;
